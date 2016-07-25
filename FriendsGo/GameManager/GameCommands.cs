3adff0712a140caf83f0841356566c331c4f6986
Ilana Kantorov <ilanak@microsoft.com>
2016-07-25 16:16:23 +0300

    Add game manager interface

---
+++ FriendsGo/GameManager/GameCommands.cs
@@ -0,0 +1,15 @@
+﻿using GoogleApi.Entities.Common;
+
+namespace GameManager
+{
+    public interface IGameCommands
+    {
+        void JoinGame(string groupId, string userId);
+
+        void StartGame(string groupId);
+
+        string GetMission(string groupId);
+
+        void CheckIn(string groupId, string userId, Location location);
+    }
+}
