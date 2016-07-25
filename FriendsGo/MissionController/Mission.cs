b88877d1ee8178164864e01a79d19fc7413f8568
Ilana Kantorov <ilanak@microsoft.com>
2016-07-25 13:46:31 +0300

    Mission Controller + 1st test

--- FriendsGo/MissionController/Mission.cs
+++ FriendsGo/MissionController/Mission.cs
@@ -8,11 +8,30 @@ namespace MissionController
 {
     public class Mission
     {
+        public Mission()
+        {
+            SubMissions = new List<SubMission>();
+        }
+
         public List<SubMission> SubMissions;
     }
 
-    public abstract class SubMission
+    public static class SubMissionsFactory
     {
+        public static SubMission Create(SubMissionType type, int level)
+        {
+            switch (type)
+            {
+                    case SubMissionType.ExactLocation:
+                        return new ExactLocationSubMission(level);
+                    default:
+                        throw new ArgumentException();
+            }
+        }
+    }
+
+    public abstract class SubMission
+    {        
     }
 
     public class ExactLocationSubMission : SubMission
@@ -22,8 +41,19 @@ namespace MissionController
         public Location  ExactLocation;
 
         public TimeSpan Duration;
+
+        public ExactLocationSubMission(int level)
+        {
+            NumberOfPlayers = level;
+            // ExactLocation = 
+            Duration = TimeSpan.MaxValue;
+        }
     }
 
-
+    public enum SubMissionType
+    {
+        Default = 0,
+        ExactLocation = 1,
+    }
     
 }
