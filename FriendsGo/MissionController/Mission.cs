5df10d29dd06fbf30a286fa6836ab919e6795b1b
Assaf Neufeld <assafn@microsoft.com>
2016-07-25 16:01:59 +0300

    Google places API

--- FriendsGo/MissionController/Mission.cs
+++ FriendsGo/MissionController/Mission.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GoogleApi;
 using GoogleApi.Entities.Common;
+using GoogleApi.Entities.Places.Search.NearBy.Request;
+using GoogleApi.Entities.Places.Search.NearBy.Response;
+using GoogleApi.Entities.Places.Search.Radar.Request;
+using GoogleApi.Entities.Places.Search.Radar.Response;
 
 namespace MissionController
 {
@@ -24,7 +29,7 @@ namespace MissionController
             switch (type)
             {
                     case SubMissionType.ExactLocation:
-                        return new ExactLocationSubMission(level);
+                        return new ExactLocationSubMission(level, startLocation);
                     default:
                         throw new ArgumentException();
             }
@@ -43,11 +48,26 @@ namespace MissionController
 
         public TimeSpan Duration;
 
-        public ExactLocationSubMission(int level)
+        public ExactLocationSubMission(int level, Location startLocation)
         {
             NumberOfPlayers = level;
-            
-            // ExactLocation = 
+
+            var placesRequest = new PlacesRadarSearchRequest()
+            {
+                Key = "AIzaSyA5t84tAgn_fgRCXM1ROaOjcEfRiMG4AZ8",
+                Sensor = true,
+                Language = "en",
+                Location = startLocation,
+                Radius = 100,
+                Keyword = "*"
+            };
+
+            var response = GooglePlaces.RadarSearch.Query(placesRequest);
+
+            var firstOrDefault = response.Results.FirstOrDefault();
+            if (firstOrDefault != null)
+                ExactLocation = firstOrDefault.Geometry.Location;
+
             Duration = TimeSpan.MaxValue;
         }
     }
