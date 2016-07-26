e4e94fe7d086538cef99fa9e105aa35f94375b8d
Michael Merjanov <mimer@microsoft.com>
2016-07-26 15:37:57 +0300

    update doc db api

--- FriendsGo/GameManager/BotUser.cs
+++ FriendsGo/GameManager/BotUser.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using GameManager;
 
 namespace Shared
 {
-    public class BotUser
+    public class BotUser : DocDbEntityBase
     {
-        public string Id { get; private set; }
         public string UserName { get; set; }
 
-        public BotUser(string id)
+        public BotUser(string id,string userName = "")
         {
-            Id = id;
+            TelegramId = id;
+            UserName = userName;
         }
     }
 }
