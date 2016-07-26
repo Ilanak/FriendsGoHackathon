6322685b7999bebb2fb3ea11b8d6b02b3db963f3
Assaf Neufeld <assafn@microsoft.com>
2016-07-26 15:29:26 +0300

    Finalizing API

--- FriendsGo/GameManagerWebApi/Controllers/GameController.cs
+++ FriendsGo/GameManagerWebApi/Controllers/GameController.cs
@@ -1,67 +1,108 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Results;
 using System.Web.Services.Protocols;
+using DocDbUtils;
 using Shared;
 
 namespace GameManagerWeb.Controllers
 {
-  
-    public interface IGameController
-    {
-        string Join(string groupId);
-
-        string Start(string groupId);
-
-        string GetMission(string groupId);
-
-        void CheckIn(string groupId, string userId, double latitude, double longitude);
-
-        void Stat(string groupId);
-    }
 
     [RoutePrefix("api/game")]
-    public class GameController : ApiController // , IGameController
+    public class GameController : ApiController 
     {
+
+        public enum UserState
+        {
+            None = 0,
+            Checkin = 1,
+            Go = 2,
+        }
+
+        public static ConcurrentDictionary<string, Tuple<string, UserState>> States = new ConcurrentDictionary<string, Tuple<string, UserState>>();
+            
+        [HttpPost]
+        [Route("{gameId}/go/{userId}")]
+        public string Go(string gameId, string userId)
+        {
+            
+
+            States[userId] = new Tuple<string, UserState>(gameId, UserState.Go);
+
+            return "";
+        }
+
         [HttpPost]
         [Route("{gameId}/join")]
         public string Join(string gameId, [FromBody] TelegramUser user)
         {
-            return $"{user.Name} has joined the game {gameId}!";
+            return "";
         }
 
         [HttpPost]
-        [Route("{gameId}/start")]
-        public string Start(string gameId)
+        [Route("cancel/{userId}")]
+        public string Cancel(string userId)
         {
-            return "Game started!";
+            States[userId] = new Tuple<string, UserState>(string.Empty, UserState.None);
+            return "";
+        }
+
+        [HttpPost]
+        [Route("{gameId}/checkin/{userId}")]
+        public string CheckIn(string gameId, string userId)
+        {
+            States[userId] = new Tuple<string, UserState>(gameId, UserState.Go);
+
+            return "";
         }
 
         [HttpGet]
         [Route("{gameId}/mission")]
         public string GetMission(string gameId)
         {
-            return $"Group {gameId} is on level ''. Your current mission is to go to BBB!";
+            var group = DocDbApi.getGroupById(gameId);
+
+            if (group != null)
+            {
+                return $"Group {group.Id} is on level {group.Level}. Your current mission is to go to BBB!";
+            }
+
+            throw new ArgumentException();
         }
 
-        public void CheckIn(string groupId, string userId, double latitude, double longitude)
+        [HttpPost]
+        [Route("location")]
+        public string Location([FromBody] UserLocation location)
         {
-            throw new System.NotImplementedException();
+            string result;
+            var userId = location.UserId;
+            if (States[userId].Item2 == UserState.Go)
+            {
+                result =  $"{userId} has GO'ed the game in {States[userId].Item1} group!";
+            }
+            else if (States[userId].Item2 == UserState.Checkin)
+            {
+                result =  $"{userId} has checked-in for game {States[userId].Item1}!";
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+
+            States[userId] = new Tuple<string, UserState>(string.Empty, UserState.None);
+            return result;
         }
 
+        [HttpGet]
+        [Route("{gameId}/stat")]
         public void Stat(string groupId)
         {
             throw new System.NotImplementedException();
-        }
-
-        public bool IsGroupExists(string groupId)
-        {
-            var user = new BotUser("exampleId1");
-            DocDbUtils.DocDbApi.CreateUser(user).Wait();
-            return true;
         }
     }
 
@@ -69,6 +110,15 @@ namespace GameManagerWeb.Controllers
     public class TelegramUser
     {
         public string Name;
+        public string Id;
+    }
+
+
+    public class UserLocation
+    {
+        public string UserId;
+        public string Latitude;
+        public string Longtitude;
     }
 
 
