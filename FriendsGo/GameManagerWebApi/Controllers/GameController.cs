60799d780556baed4941b3db543286176a9b7279
Assaf Neufeld <assafn@microsoft.com>
2016-07-26 13:49:08 +0300

    Removing BOT. Turning game manager into web API.

---
+++ FriendsGo/GameManagerWebApi/Controllers/GameController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Services.Protocols;
+
+namespace GameManagerWeb.Controllers
+{
+  
+    public interface IGameController
+    {
+        string Join(string groupId);
+
+        string Start(string groupId);
+
+        string GetMission(string groupId);
+
+        void CheckIn(string groupId, string userId, double latitude, double longitude);
+
+        void Stat(string groupId);
+    }
+
+    [RoutePrefix("api/game")]
+    public class GameController : ApiController // , IGameController
+    {
+        [HttpPost]
+        [Route("{gameId:int}/join")]
+        public string Join(int gameId, [FromBody] string userName)
+        {
+            return $"{userName} has joined the game {gameId}!";
+        }
+
+        [HttpPost]
+        public string Start(string groupId)
+        {
+            return "Game started!";
+        }
+
+        public string GetMission(string groupId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CheckIn(string groupId, string userId, double latitude, double longitude)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Stat(string groupId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
