c6a8c225acb32afb6a854463c93f79368d6dfee6
rlemberg <rlemberg@microsoft.com>
2016-07-27 13:17:16 +0300

    add mission logic

--- FriendsGo/GameManager/MissionController/IMissionController.cs
+++ FriendsGo/GameManager/MissionController/IMissionController.cs
@@ -16,30 +16,37 @@ namespace GameManager
 
     public class MissionController : IMissionController
     {
-        public Dictionary<int, int> Levels;
+        public Dictionary<int, Tuple<int, int, int, int, bool>> LevelsDifficulty;
 
         public MissionController()
         {
-            //this describes how many different submissions a mission will hold depending on the current level
-            Levels = new Dictionary<int, int>();
-            Levels[0] = 1;
-            Levels[1] = 1;
-            Levels[2] = 1;
-            Levels[3] = 2;
-            Levels[4] = 2;
-            Levels[5] = 2;
-            Levels[6] = 2;
-            Levels[7] = 2;
-            Levels[8] = 3;
+            //this describes the levels difficulty
+            //int - how many different submissions a mission will hold
+            //int - how many checkin are required for *each* submission
+            //int - a meter based radius for the submission to select location within
+            //int - sec duration for checkins to happen 
+            //bool - flag add city - randomize a new city to add to game
+            LevelsDifficulty = new Dictionary<int, Tuple<int, int, int, int, bool>>();
+            LevelsDifficulty[0] = new Tuple<int, int, int, int, bool>(1, 1, 10,     600, false);
+            LevelsDifficulty[1] = new Tuple<int, int, int, int, bool>(1, 2, 10,     600, false);
+            LevelsDifficulty[2] = new Tuple<int, int, int, int, bool>(1, 2, 50,     600, false);
+            LevelsDifficulty[3] = new Tuple<int, int, int, int, bool>(1, 3, 10,     600, false);
+            LevelsDifficulty[4] = new Tuple<int, int, int, int, bool>(1, 3, 100,    600, false);
+            LevelsDifficulty[5] = new Tuple<int, int, int, int, bool>(2, 1, 10,     600, false);
+            LevelsDifficulty[6] = new Tuple<int, int, int, int, bool>(2, 1, 200,    600, false);
+            LevelsDifficulty[7] = new Tuple<int, int, int, int, bool>(2, 2, 100,    600, false);
+            LevelsDifficulty[8] = new Tuple<int, int, int, int, bool>(2, 2, 200,    600, false);
+            LevelsDifficulty[9] = new Tuple<int, int, int, int, bool>(3, 1, 10,     600, true);
+
         }
 
         public Mission GetMission(int level, Location startLocation, List<Location> previousLocations)
         {
             var mission = new Mission();
 
-            for (int i = 0; i < Levels[level]; i++)
+            for (int i = 0; i < (LevelsDifficulty[level].Item1); i++)
             {
-                var submission = SubMissionsFactory.Create(SubMissionType.ExactLocation, level, startLocation);
+                var submission = SubMissionsFactory.Create(SubMissionType.ExactLocation, level, startLocation, LevelsDifficulty[level].Item2, LevelsDifficulty[level].Item3);
 
                 mission.SubMissions.Add(submission);
             }
