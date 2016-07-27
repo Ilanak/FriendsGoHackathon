0c3d4e4ca6c7f0093cb477205667a42cf12796f3
rlemberg <rlemberg@microsoft.com>
2016-07-27 11:40:31 +0300

    add check for mission complete

--- FriendsGo/GameManager/MissionController/IMissionController.cs
+++ FriendsGo/GameManager/MissionController/IMissionController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,19 +20,29 @@ namespace GameManager
 
         public MissionController()
         {
+            //this describes how many different submissions a mission will hold depending on the current level
             Levels = new Dictionary<int, int>();
             Levels[0] = 1;
-            Levels[1] = 2;
-            Levels[2] = 3;
+            Levels[1] = 1;
+            Levels[2] = 1;
+            Levels[3] = 2;
+            Levels[4] = 2;
+            Levels[5] = 2;
+            Levels[6] = 2;
+            Levels[7] = 2;
+            Levels[8] = 3;
         }
 
         public Mission GetMission(int level, Location startLocation, List<Location> previousLocations)
         {
             var mission = new Mission();
 
-            var submission = SubMissionsFactory.Create(SubMissionType.ExactLocation, level, startLocation);
-            
-            mission.SubMissions.Add(submission);
+            for (int i = 0; i < Levels[level]; i++)
+            {
+                var submission = SubMissionsFactory.Create(SubMissionType.ExactLocation, level, startLocation);
+
+                mission.SubMissions.Add(submission);
+            }
 
             return mission;
         }
