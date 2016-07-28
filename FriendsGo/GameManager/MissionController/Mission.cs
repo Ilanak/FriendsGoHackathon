9d99b9c202d8b32bb71ffa0370269f72a5319ea9
rlemberg <rlemberg@microsoft.com>
2016-07-28 13:16:25 +0300

    check in continue

--- FriendsGo/GameManager/MissionController/Mission.cs
+++ FriendsGo/GameManager/MissionController/Mission.cs
@@ -121,6 +121,10 @@ namespace GameManager
 
         public override bool ValidateLocation(Location userLocation, string userId, bool debugMode = false)
         {
+            if (IsCompleted())
+            {
+                return false;
+            }
             if (!CheckIns.ContainsKey(userId) && ValidateLocation(userLocation))
             {
                 CheckIns[userId] = userLocation;
